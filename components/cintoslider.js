db4cd022231959fcf2da0bec0e9a63b4fea72638
1uciferx <akhil@shadobooks.com>
2023-03-27 15:48:28 +0530

    Update cintoslider.js

--- components/cintoslider.js
+++ components/cintoslider.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 import Image from "next/image";
-import styles from "../styles/Cintoslider.module.css";
+import styles from "../styles/cintoslider.module.css";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
