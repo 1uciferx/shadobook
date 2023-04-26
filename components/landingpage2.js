99f143c09e2f18578139914a9cc64d54c4e847fd
phoenixsparrow <120713598+phoenixsparrow@users.noreply.github.com>
2023-04-26 12:26:10 +0530

    by abith on 26-4-23

--- components/landingpage2.js
+++ components/landingpage2.js
@@ -5,22 +5,19 @@ import styles from "../styles/landingpage2.module.css";
 function Landingpage2() {
   return (
     <>
-      <div className="row">
-        <div className="col-lg-12">
+     
+          <div className={styles.qhead}>
           <h2 className={styles.mainhead}>Industries We are Serving For</h2>
-        </div>
+      
       </div>
 
-      <div className="row">
-        <div className="col-lg-12">
-          <div className={styles.smallheading}>
-            Shadobooks ERP provides a comprehensive set of features and tools to
+          <div className={styles.phead}>
+            <p  className={styles.smallheading}>Shadobooks ERP provides a comprehensive set of features and tools to
             manage different aspects of a company's operations, such as
             inventory management, financial accounting, sales and purchase order
             management, production planning, and customer relationship
-            management.
-          </div>
-        </div>
+            management.</p>
+        
       </div>
     </>
   );
