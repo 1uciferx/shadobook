11c11bbf8e35ce503d56aa4124fbcdf990379cfe
phoenixsparrow <abith@shadobooks.com>
2023-06-01 16:04:07 +0530

    gtag changes

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,7 @@ function MyApp({ Component, pageProps }) {
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       dataLayer.push(arguments);
-    }
+    };
     gtag('js', new Date());
     gtag('config', 'AW-11183033842');
 
